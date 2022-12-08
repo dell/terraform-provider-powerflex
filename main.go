f94e369c7c0ef5ec96d7fffc118e668f36ce6e1d
AnshumanPradipPatil1506 <AnshumanPradip.Patil@dellteam.com>
2022-12-08 03:29:50 -0500

    added initial files - [wip] refractor folder structure needed

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"log"
 	"terraform-provider-powerflex/powerflex"
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-)
 
-var (
-	version string = "dev"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	var debug bool
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-	err := providerserver.Serve(context.Background(), powerflex.New, providerserver.ServeOpts{
-		Address: "dell.com/ses/powerflex",
-		Debug:   debug,
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return powerflex.Provider()
+		},
 	})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 }
+
+// func main() {
+// 	var debug bool
+// 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+// 	flag.Parse()
+// 	err := providerserver.Serve(context.Background(), powerflex.Provider, providerserver.ServeOpts{
+// 		Address: "dell.com/ses/powerflex",
+// 		Debug:   debug,
+// 	})
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+// }
