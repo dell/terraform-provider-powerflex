eebf33b350a219ffabc340e371fa527d54946943
Krishnan-Priyanshu <Priyanshu.Krishnan@dell.com>
2022-12-21 00:40:38 +0530

    ♻️ refactor the volume resource

---
+++ powerflex/volume/volume_resource.helper.go
@@ -0,0 +1,89 @@
+package volume
+
+import (
+	pftypes "github.com/dell/goscaleio/types/v1"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func updateVolumeState(vol *pftypes.Volume, plan volumeResourceModel) (response volumeResourceModel) {
+	response = volumeResourceModel{
+		ProtectionDomainID:                 plan.ProtectionDomainID,
+		VolumeSizeInKb:                     plan.VolumeSizeInKb,
+		StoragePoolID:                      types.StringValue(vol.StoragePoolID),
+		UseRmCache:                         types.BoolValue(vol.UseRmCache),
+		MappingToAllSdcsEnabled:            types.BoolValue(vol.MappingToAllSdcsEnabled),
+		IsObfuscated:                       types.BoolValue(vol.IsObfuscated),
+		VolumeType:                         types.StringValue(vol.VolumeType),
+		ConsistencyGroupID:                 types.StringValue(vol.ConsistencyGroupID),
+		VTreeID:                            types.StringValue(vol.VTreeID),
+		AncestorVolumeID:                   types.StringValue(vol.AncestorVolumeID),
+		MappedScsiInitiatorInfo:            types.StringValue(vol.MappedScsiInitiatorInfo),
+		SizeInKb:                           types.Int64Value(int64(vol.SizeInKb)),
+		CreationTime:                       types.Int64Value(int64(vol.CreationTime)),
+		Name:                               types.StringValue(vol.Name),
+		ID:                                 types.StringValue(vol.ID),
+		DataLayout:                         types.StringValue(vol.DataLayout),
+		NotGenuineSnapshot:                 types.BoolValue(vol.NotGenuineSnapshot),
+		AccessModeLimit:                    types.StringValue(vol.AccessModeLimit),
+		SecureSnapshotExpTime:              types.Int64Value(int64(vol.SecureSnapshotExpTime)),
+		ManagedBy:                          types.StringValue(vol.ManagedBy),
+		LockedAutoSnapshot:                 types.BoolValue(vol.LockedAutoSnapshot),
+		LockedAutoSnapshotMarkedForRemoval: types.BoolValue(vol.LockedAutoSnapshotMarkedForRemoval),
+		CompressionMethod:                  types.StringValue(vol.CompressionMethod),
+		TimeStampIsAccurate:                types.BoolValue(vol.TimeStampIsAccurate),
+		OriginalExpiryTime:                 types.Int64Value(int64(vol.OriginalExpiryTime)),
+		VolumeReplicationState:             types.StringValue(vol.VolumeReplicationState),
+		ReplicationJournalVolume:           types.BoolValue(vol.ReplicationJournalVolume),
+		ReplicationTimeStamp:               types.Int64Value(int64(vol.ReplicationTimeStamp)),
+	}
+	linkAttrTypes := map[string]attr.Type{
+		"rel":  types.StringType,
+		"href": types.StringType,
+	}
+	mappedSdcInfoAttrTypes := map[string]attr.Type{
+		"sdc_id":                   types.StringType,
+		"sdc_ip":                   types.StringType,
+		"limit_iops":               types.Int64Type,
+		"limit_bw_in_mbps":         types.Int64Type,
+		"sdc_name":                 types.StringType,
+		"access_mode":              types.StringType,
+		"is_direct_buffer_mapping": types.BoolType,
+	}
+	linkElemType := types.ObjectType{
+		AttrTypes: linkAttrTypes,
+	}
+	mappedSdcInfoElemType := types.ObjectType{
+		AttrTypes: mappedSdcInfoAttrTypes,
+	}
+	objectLinks := []attr.Value{}
+	objectMappedSdcInfos := []attr.Value{}
+
+	for _, link := range vol.Links {
+		obj := map[string]attr.Value{
+			"rel":  types.StringValue(link.Rel),
+			"href": types.StringValue(link.HREF),
+		}
+		objVal, _ := types.ObjectValue(linkAttrTypes, obj)
+		objectLinks = append(objectLinks, objVal)
+	}
+	listVal, _ := types.ListValue(linkElemType, objectLinks)
+
+	for _, msi := range vol.MappedSdcInfo {
+		obj := map[string]attr.Value{
+			"sdc_id":                   types.StringValue(msi.SdcID),
+			"sdc_ip":                   types.StringValue(msi.SdcIP),
+			"limit_iops":               types.Int64Value(int64(msi.LimitIops)),
+			"limit_bw_in_mbps":         types.Int64Value(int64(msi.LimitBwInMbps)),
+			"sdc_name":                 types.StringValue(msi.SdcName),
+			"access_mode":              types.StringValue(msi.AccessMode),
+			"is_direct_buffer_mapping": types.BoolValue(msi.IsDirectBufferMapping),
+		}
+		objVal, _ := types.ObjectValue(mappedSdcInfoAttrTypes, obj)
+		objectMappedSdcInfos = append(objectMappedSdcInfos, objVal)
+	}
+	mappedSdcInfoVal, _ := types.ListValue(mappedSdcInfoElemType, objectMappedSdcInfos)
+	response.Links = listVal
+	response.MappedSdcInfo = mappedSdcInfoVal
+	return response
+}
---
+++ powerflex/volume/volume_resource.model.go
@@ -0,0 +1,53 @@
+package volume
+
+import "github.com/hashicorp/terraform-plugin-framework/types"
+
+// volumeResourceModel maps the resource schema data.
+type volumeResourceModel struct {
+	ProtectionDomainID                 types.String `tfsdk:"protection_domain_id"`
+	StoragePoolID                      types.String `tfsdk:"storage_pool_id"`
+	VolumeType                         types.String `tfsdk:"volume_type"`
+	UseRmCache                         types.Bool   `tfsdk:"use_rm_cache"`
+	VolumeSizeInKb                     types.String `tfsdk:"volume_size_in_kb"`
+	Name                               types.String `tfsdk:"name"`
+	MappingToAllSdcsEnabled            types.Bool   `tfsdk:"mapping_to_all_sdcs_enabled"`
+	IsObfuscated                       types.Bool   `tfsdk:"is_obfuscated"`
+	ConsistencyGroupID                 types.String `tfsdk:"consistency_group_id"`
+	VTreeID                            types.String `tfsdk:"vtree_id"`
+	AncestorVolumeID                   types.String `tfsdk:"ancestor_volume_id"`
+	MappedScsiInitiatorInfo            types.String `tfsdk:"mapped_scsi_initiator_info"`
+	SizeInKb                           types.Int64  `tfsdk:"size_in_kb"`
+	CreationTime                       types.Int64  `tfsdk:"creation_time"`
+	ID                                 types.String `tfsdk:"id"`
+	DataLayout                         types.String `tfsdk:"data_layout"`
+	NotGenuineSnapshot                 types.Bool   `tfsdk:"not_genuine_snapshot"`
+	AccessModeLimit                    types.String `tfsdk:"access_mode_limit"`
+	SecureSnapshotExpTime              types.Int64  `tfsdk:"secure_snapshot_exp_time"`
+	ManagedBy                          types.String `tfsdk:"managed_by"`
+	LockedAutoSnapshot                 types.Bool   `tfsdk:"locked_auto_snapshot"`
+	LockedAutoSnapshotMarkedForRemoval types.Bool   `tfsdk:"locked_auto_snapshot_marked_for_removal"`
+	CompressionMethod                  types.String `tfsdk:"compression_method"`
+	TimeStampIsAccurate                types.Bool   `tfsdk:"time_stamp_is_accurate"`
+	OriginalExpiryTime                 types.Int64  `tfsdk:"original_expiry_time"`
+	VolumeReplicationState             types.String `tfsdk:"volume_replication_state"`
+	ReplicationJournalVolume           types.Bool   `tfsdk:"replication_journal_volume"`
+	ReplicationTimeStamp               types.Int64  `tfsdk:"replication_time_stamp"`
+	Links                              types.List   `tfsdk:"links"`
+	MappedSdcInfo                      types.List   `tfsdk:"mapped_sdc_info"`
+}
+
+type MappedSdcInfo struct {
+	SdcID                 types.String `tfsdk:"sdc_id"`
+	SdcIP                 types.String `tfsdk:"sdc_ip"`
+	LimitIops             types.Int64  `tfsdk:"limit_iops"`
+	LimitBwInMbps         types.Int64  `tfsdk:"limit_bw_in_mbps"`
+	SdcName               types.String `tfsdk:"sdc_name"`
+	AccessMode            types.String `tfsdk:"access_mode"`
+	IsDirectBufferMapping types.Bool   `tfsdk:"is_direct_buffer_mapping"`
+}
+
+// Link defines struct of Link
+type Link struct {
+	Rel  types.String `tfsdk:"rel"`
+	HREF types.String `tfsdk:"href"`
+}
