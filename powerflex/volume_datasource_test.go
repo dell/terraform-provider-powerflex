54eab038944fb1dc401f222ca821861bc365dedf
AnikaAgiwal2711 <anika.agiwal@dell.com>
2022-12-26 05:02:33 -0500

    added provider test file and worked on Akash's comments

--- powerflex/volume_datasource_test.go
+++ powerflex/volume_datasource_test.go
@@ -1,21 +1,10 @@
 package powerflex
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"os"
 	"testing"
 )
-
-const providerConfigForTesting = `
-	provider "powerflex" {
-	username = ""
-    password = ""
-    endpoint = ""
-
-	}
-`
 
 type dataPoints struct {
 	name          string
@@ -41,16 +30,12 @@ func init() {
 func TestAccVolumeDataSource(t *testing.T) {
 	os.Setenv("TF_ACC", "1")
 	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
-			"powerflex": providerserver.NewProtocol6WithError(New()),
-		},
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			//retrieving volume based on id
 			{
-				Config: providerConfigForTesting + `
-				data "powerflex_volume" "all" {						
-						id = "457752ff000000c7"
-					}`,
+				Config: VolumeDataSourceConfig1,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the first volume to ensure attributes are correctly set
 					resource.TestCheckResourceAttr("data.powerflex_volume.all", "volumes.0.id", volumeTestData.id),
@@ -62,10 +47,7 @@ func TestAccVolumeDataSource(t *testing.T) {
 			},
 			//retrieving volume based on name
 			{
-				Config: providerConfigForTesting + `
-				data "powerflex_volume" "all" {						
-						name = "cicd-dbc5a5909d"
-					}`,
+				Config: VolumeDataSourceConfig2,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the first volume to ensure attributes are correctly set
 					resource.TestCheckResourceAttr("data.powerflex_volume.all", "volumes.0.id", volumeTestData.id),
@@ -77,10 +59,7 @@ func TestAccVolumeDataSource(t *testing.T) {
 			},
 			//retrieving volume based on storage pool id
 			{
-				Config: providerConfigForTesting + `
-				data "powerflex_volume" "all" {						
-						storage_pool_id = "7630a24600000000"
-					}`,
+				Config: VolumeDataSourceConfig3,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the volume to ensure storage pool id attributes is correctly set
 					resource.TestCheckResourceAttr("data.powerflex_volume.all", "volumes.0.storage_pool_id", volumeTestData.storagePoolID),
@@ -88,10 +67,7 @@ func TestAccVolumeDataSource(t *testing.T) {
 			},
 			//retrieving volume based on storage pool name
 			{
-				Config: providerConfigForTesting + `
-				data "powerflex_volume" "all" {						
-						storage_pool_name = "pool1"
-					}`,
+				Config: VolumeDataSourceConfig4,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the volume to ensure storage pool id attributes is correctly set
 					resource.TestCheckResourceAttr("data.powerflex_volume.all", "volumes.0.storage_pool_id", volumeTestData.storagePoolID),
@@ -99,9 +75,7 @@ func TestAccVolumeDataSource(t *testing.T) {
 			},
 			//retrieving all the volumes
 			{
-				Config: providerConfigForTesting + `
-				data "powerflex_volume" "all" {						
-					}`,
+				Config: VolumeDataSourceConfig5,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the volume to ensure all attributes are set
 					resource.TestCheckResourceAttr("data.powerflex_volume.all", "volumes.0.storage_pool_id", volumeTestData.storagePoolID),
@@ -110,3 +84,62 @@ func TestAccVolumeDataSource(t *testing.T) {
 		},
 	})
 }
+
+var VolumeDataSourceConfig1 = `
+provider "powerflex" {
+	username = "` + username + `"
+	password = "` + password + `"
+	endpoint = "` + endpoint + `"
+	insecure = true
+}
+data "powerflex_volume" "all" {						
+	id = "457752ff000000c7"
+}
+`
+
+var VolumeDataSourceConfig2 = `
+provider "powerflex" {
+	username = "` + username + `"
+	password = "` + password + `"
+	endpoint = "` + endpoint + `"
+	insecure = true
+}
+data "powerflex_volume" "all" {						
+	name = "cicd-dbc5a5909d"
+}
+`
+
+var VolumeDataSourceConfig3 = `
+provider "powerflex" {
+	username = "` + username + `"
+	password = "` + password + `"
+	endpoint = "` + endpoint + `"
+	insecure = true
+}
+data "powerflex_volume" "all" {						
+	storage_pool_id = "7630a24600000000"
+}
+`
+
+var VolumeDataSourceConfig4 = `
+provider "powerflex" {
+	username = "` + username + `"
+	password = "` + password + `"
+	endpoint = "` + endpoint + `"
+	insecure = true
+}
+data "powerflex_volume" "all" {						
+	storage_pool_name = "pool1"
+}
+`
+
+var VolumeDataSourceConfig5 = `
+provider "powerflex" {
+	username = "` + username + `"
+	password = "` + password + `"
+	endpoint = "` + endpoint + `"
+	insecure = true
+}
+data "powerflex_volume" "all" {						
+}
+`
