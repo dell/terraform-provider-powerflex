fe7d262101178bfa545229f01dde7401b3a5d6c0
Krishnan-Priyanshu <Priyanshu.Krishnan@dell.com>
2023-01-10 07:50:14 +0530

    🌱 snapshot example with new fields added

--- examples/snapshot.tf
+++ examples/snapshot.tf
@@ -5,8 +5,27 @@ resource "powerflex_snapshot" "snapshots-create" {
 }
 
 resource "powerflex_snapshot" "snapshots-create-1" {
-	name = "<snapshot-name>"
-	volume_id = "<volume-id>"
-	access_mode = "<access-mode>"
-	map_sdcs_id = ["<sdc-id-1>","<sdc-id-2>"]
+	name = "<snapshot name>"
+	volume_name = "<volume name>"
+	access_mode = "<access mode options are ReadOnly/ReadWrite, default value ReadOnly>"
+	size = "<size[int] associated with capacity unit>"
+	capacity_unit =  "<capacity unit options are gb/tb, default value gb>"
+	lock_auto_snapshot = "<lock auto snapshot, snapshot which are created by snapshot policy can be locked.>"
+	desired_retention = "<desired retention[int] associated with retention unit>"
+	retention_unit = "<retention unit options are hours/days, default value hours>"
+	remove_mode = "<remove mode options are ONLY_ME/INCLUDING_DESCENDANTS, default value ONLY_ME>"
+	sdc_list = [
+	   {
+		sdc_id = "<sdc id either of sdc_id/sdc_name should be present to map snapshot to sdc>"
+		limit_iops = "<limit iops setting on mapped sdc>"
+		limit_bw_in_mbps = "<limit bw in mbps setting on mapped sdc>"
+		access_mode = "<access mode options are ReadOnly/ReadWrite, default value ReadOnly>"
+	   },
+	   	   {
+		sdc_name = "<sdc name either of sdc_id/sdc_name should be present to map snapshot to sdc>"
+		limit_iops = "<limit iops setting on mapped sdc>"
+		limit_bw_in_mbps = "<limit bw in mbps setting on mapped sdc>"
+		access_mode = "<access mode options are ReadOnly/ReadWrite, default value ReadOnly>"
+	   }
+	]
 }
