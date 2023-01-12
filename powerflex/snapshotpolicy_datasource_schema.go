d05732b81c47cd7ccbe9760a72ff7a4223045032
AnikaAgiwal2711 <anika.agiwal@dell.com>
2023-01-12 07:09:29 -0500

    Implemented the functionality for snapshot policy data source

---
+++ powerflex/snapshotpolicy_datasource_schema.go
@@ -0,0 +1,154 @@
+package powerflex
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// VolumeDataSourceSchema is the schema for reading the volume data
+var SnapshotPolicyDataSourceSchema schema.Schema = schema.Schema{
+	Description: ".",
+	Attributes: map[string]schema.Attribute{
+		"id": schema.StringAttribute{
+			Description:         "Unique identifier of the snapshot policy instance.",
+			MarkdownDescription: "Unique identifier of the snapshot policy instance.",
+			Optional:            true,
+			Computed:            true,
+		},
+		"name": schema.StringAttribute{
+			Description:         "Name of the snapshot policy.",
+			MarkdownDescription: "Name of the snapshot policy.",
+			Optional:            true,
+			Validators: []validator.String{
+				stringvalidator.ConflictsWith(path.MatchRoot("id")),
+			},
+		},
+		"snapshotpolicies": schema.ListNestedAttribute{
+			Description:         "List of snapshot policies.",
+			MarkdownDescription: "List of snapshot policies.",
+			Computed:            true,
+			NestedObject: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"id": schema.StringAttribute{
+						Description:         "Unique identifier of the snapshot policy instance.",
+						MarkdownDescription: "Unique identifier of the snapshot policy instance.",
+						Computed:            true,
+					},
+					"name": schema.StringAttribute{
+						Description:         "Name of the snapshot policy.",
+						MarkdownDescription: "Name of the snapshot policy.",
+						Computed:            true,
+					},
+					"snapshot_policy_state": schema.StringAttribute{
+						Description:         "Specifies the current state of the snapshot policy.",
+						MarkdownDescription: "Specifies the current state of the snapshot policy.",
+						Computed:            true,
+					},
+					"auto_snapshot_creation_cadence_in_min": schema.Int64Attribute{
+						Description:         "Auto snapshot creation cadence in min.",
+						MarkdownDescription: "Auto snapshot creation cadence in min.",
+						Computed:            true,
+					},
+					"max_vtree_auto_snapshots": schema.Int64Attribute{
+						Description:         "Max vtree auto snapshots.",
+						MarkdownDescription: "Max vtree auto snapshots.",
+						Computed:            true,
+					},
+					"num_of_source_volumes": schema.Int64Attribute{
+						Description:         "Number of source Volumes.",
+						MarkdownDescription: "Number of source Volumes.",
+						Computed:            true,
+					},
+					"num_of_expired_but_locked_snapshots": schema.Int64Attribute{
+						Description:         "Number of expired but locked snapshots.",
+						MarkdownDescription: "Number of expired but locked snapshots.",
+						Computed:            true,
+					},
+					"num_of_creation_failures": schema.Int64Attribute{
+						Description:         "Number of creation failures.",
+						MarkdownDescription: "Number of creation failures.",
+						Computed:            true,
+					},
+					"num_of_retained_snapshots_per_level": schema.ListAttribute{
+						ElementType:         types.Int64Type,
+						Description:         "Number of retained snapshots per level.",
+						MarkdownDescription: "Number of retained snapshots per level.",
+						Computed:            true,
+					},
+					"snapshot_access_mode": schema.StringAttribute{
+						Description:         "Snapshot Access Mode.",
+						MarkdownDescription: "Snapshot Access Mode.",
+						Computed:            true,
+					},
+					"secure_snapshots": schema.BoolAttribute{
+						Description:         "Secure snapshots.",
+						MarkdownDescription: "Secure snapshots.",
+						Computed:            true,
+					},
+					"time_of_last_auto_snapshot": schema.Int64Attribute{
+						Description:         "Time of last auto snapshot.",
+						MarkdownDescription: "Time of last auto snapshot.",
+						Computed:            true,
+					},
+					"next_auto_snapshot_creation_time": schema.Int64Attribute{
+						Description:         "Next auto snapshot creation time.",
+						MarkdownDescription: "Next auto snapshot creation time.",
+						Computed:            true,
+					},
+					"time_of_last_auto_snapshot_creation_failure": schema.Int64Attribute{
+						Description:         "Time of last auto snapshot creation failure.",
+						MarkdownDescription: "Time of last auto snapshot creation failure.",
+						Computed:            true,
+					},
+					"last_auto_snapshot_creation_failure_reason": schema.StringAttribute{
+						Description:         "Last auto snapshot creation failure reason.",
+						MarkdownDescription: "Last auto snapshot creation failure reason.",
+						Computed:            true,
+					},
+					"last_auto_snapshot_failure_in_first_level": schema.BoolAttribute{
+						Description:         "Last auto snapshot failure in first level.",
+						MarkdownDescription: "Last auto snapshot failure in first level.",
+						Computed:            true,
+					},
+					"num_of_auto_snapshots": schema.Int64Attribute{
+						Description:         "Number of auto snapshots.",
+						MarkdownDescription: "Number of auto snapshots.",
+						Computed:            true,
+					},
+					"num_of_locked_snapshots": schema.Int64Attribute{
+						Description:         "Number of locked snapshots.",
+						MarkdownDescription: "Number of locked snapshots.",
+						Computed:            true,
+					},
+					"system_id": schema.StringAttribute{
+						Description:         "System Identifier.",
+						MarkdownDescription: "System Identifier.",
+						Computed:            true,
+					},
+					"links": schema.ListNestedAttribute{
+						Description:         "Specifies the links asscociated for a volume.",
+						MarkdownDescription: "Specifies the links asscociated for a volume.",
+						Computed:            true,
+						NestedObject: schema.NestedAttributeObject{
+							Attributes: map[string]schema.Attribute{
+								"rel": schema.StringAttribute{
+									Description:         "Specifies the relationship with the volume.",
+									MarkdownDescription: "Specifies the relationship with the volume.",
+									Computed:            true,
+								},
+								"href": schema.StringAttribute{
+									Description:         "Specifies the exact path to fetch the details.",
+									MarkdownDescription: "Specifies the exact path to fetch the details.",
+									Computed:            true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
---
+++ powerflex/snapshotpolicy_datasource_test.go
@@ -0,0 +1,89 @@
+package powerflex
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"os"
+	"testing"
+	"regexp"
+)
+
+// TestAccSnapshotPolicyDataSource tests the snapshot policy data source
+// where it fetches the snapshot policies based on snapshot policy id/name 
+// and if nothing is mentioned , then return all snapshot policies
+func TestAccSnapshotPolicyDataSource(t *testing.T) {
+	os.Setenv("TF_ACC", "1")
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			//retrieving snapshot policy based on id
+			{
+				Config: ProviderConfigForTesting + SnapshotPolicyDataSourceConfig1,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify the first snapshot policy to ensure attributes are correctly set
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp1", "snapshotpolicies.0.id", "15ad99b900000001"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp1", "snapshotpolicies.0.name", "sample_snap_policy_1"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp1", "snapshotpolicies.#", "1"),
+				),
+			},
+			//retrieving snapshot policy based on name
+			{
+				Config: ProviderConfigForTesting + SnapshotPolicyDataSourceConfig2,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify the first snapshot policy to ensure attributes are correctly set
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp2", "snapshotpolicies.0.id", "15ad99b900000001"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp2", "snapshotpolicies.0.name", "sample_snap_policy_1"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp2", "snapshotpolicies.#", "1"),
+				),
+			},
+			//retrieving all snapshot policies
+			{
+				Config: ProviderConfigForTesting + SnapshotPolicyDataSourceConfig3,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify the first snapshot policy to ensure attributes are correctly set
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp3", "snapshotpolicies.0.id", "15ad99b800000000"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp3", "snapshotpolicies.#", "4"),
+					resource.TestCheckResourceAttr("data.powerflex_snapshotpolicy.sp3", "snapshotpolicies.0.name", "sample_snap_policy"),
+				),
+			},
+			//retrieving snapshot policy with empty snapshot policy id
+			{
+				Config:      ProviderConfigForTesting + SnapshotPolicyDataSourceConfig4,
+				ExpectError: regexp.MustCompile(".*Unable to Read Powerflex Snapshot Policy.*"),
+			},	
+			//retrieving snapshot policy with incorrect snapshot policy id
+			{
+				Config:      ProviderConfigForTesting + SnapshotPolicyDataSourceConfig5,
+				ExpectError: regexp.MustCompile(".*Unable to Read Powerflex Snapshot Policy.*"),
+			},
+		},
+	})
+}
+
+var SnapshotPolicyDataSourceConfig1 = `
+data "powerflex_snapshotpolicy" "sp1" {						
+	id = "15ad99b900000001"
+}
+`
+
+var SnapshotPolicyDataSourceConfig2 = `
+data "powerflex_snapshotpolicy" "sp2" {						
+	name = "sample_snap_policy_1"
+}
+`
+
+var SnapshotPolicyDataSourceConfig3 = `
+data "powerflex_snapshotpolicy" "sp3" {						
+}
+`
+
+var SnapshotPolicyDataSourceConfig4 = `
+data "powerflex_snapshotpolicy" "sp4" {						
+	id = ""
+}
+`
+
+var SnapshotPolicyDataSourceConfig5 = `
+data "powerflex_snapshotpolicy" "sp5" {	
+	id = "15ad99b9000"					
+}
+`
