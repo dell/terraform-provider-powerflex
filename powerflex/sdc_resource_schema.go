a574551b7fb64a9900020b8f68232de4d2e2f198
AnikaAgiwal2711 <anika.agiwal@dell.com>
2023-01-20 06:26:44 -0500

    formatting issues fixed

--- powerflex/sdc_resource_schema.go
+++ powerflex/sdc_resource_schema.go
@@ -1,8 +1,8 @@
 package powerflex
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
