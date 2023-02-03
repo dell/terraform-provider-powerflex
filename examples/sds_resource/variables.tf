52de4c0f6de8de6cdc8235ee612c311185db99ce
AnikaAgiwal2711 <anika.agiwal@dell.com>
2023-02-03 10:43:54 -0500

    removed confidential data

--- examples/sds_resource/variables.tf
+++ examples/sds_resource/variables.tf
@@ -1,17 +1,14 @@
 variable "username" {
   type = string
   description = "Stores the username of PowerFlex host."
-  default = "admin"
 }
 
 variable "password" {
   type = string
   description = "Stores the password of PowerFlex host."
-  default = "Password123"
 }
 
 variable "endpoint" {
     type = string
     description = "Stores the endpoint of PowerFlex host"
-    default = "https://10.247.101.69"
 }
