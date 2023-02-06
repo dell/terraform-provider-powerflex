8bf16c917d33b95a2743efa8d1017ac94e7caff6
Rounak Adhikary <Rounak.Adhikary@dell.com>
2023-02-06 06:45:09 -0500

    Prevent one IP from being configured with multiple roles in SDS

--- powerflex/sds_resource.go
+++ powerflex/sds_resource.go
@@ -128,6 +128,37 @@ func sdsIPListDiff(ctx context.Context, plan, state *sdsResourceModel) (toAdd, t
 	return toAdd, toRmv, changed, common
 }
 
+func (r *sdsResource) ValidateConfig(ctx context.Context, req resource.ValidateConfigRequest, resp *resource.ValidateConfigResponse) {
+	var data sdsResourceModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	iplist := data.getIPList(ctx)
+	ipmap := make(map[string]int)
+	for _, ipObj := range iplist {
+		if _, ok := ipmap[ipObj.IP]; ok {
+			ipmap[ipObj.IP]++
+		} else {
+			ipmap[ipObj.IP] = 1
+		}
+	}
+
+	for ip, count := range ipmap {
+		if count == 1 {
+			continue
+		}
+		resp.Diagnostics.AddAttributeError(
+			path.Root("ip_list"),
+			"IP Duplication Error",
+			fmt.Sprintf("The IP %s is configured with %d roles, but only 1 role expected.", ip, count),
+		)
+	}
+}
+
 // Create creates the resource and sets the initial Terraform state.
 func (r *sdsResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	// Retrieve values from plan
