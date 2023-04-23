00c0622f49851330ee3070264a60ae4357bc78ae
Priyanshu Krishnan <priyanshu.krishnan@dell.com>
2023-04-23 09:46:23 -0400

    sdc validation

--- powerflex/sdc_datasource_test.go
+++ powerflex/sdc_datasource_test.go
@@ -90,7 +90,7 @@ func TestSdcDataSourceByName(t *testing.T) {
 				Config: ProviderConfigForTesting + TestSdcDataSourceByNameBlock,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify number of sdc returned
-					resource.TestCheckResourceAttr("data.powerflex_sdc.selected", "id", SdsResourceTestData.sdcName3),
+					resource.TestCheckResourceAttr("data.powerflex_sdc.selected", "name", SdsResourceTestData.sdcName3),
 				),
 			},
 		},
