8fa39872023c287e73b8d519196c76bef20c6e2e
Rounak Adhikary <Rounak.Adhikary@dell.com>
2023-04-26 12:51:15 -0400

    Add planmodifiers to reduce the number of 'known-after-apply's

--- powerflex/protection_domain_resource_schema.go
+++ powerflex/protection_domain_resource_schema.go
@@ -4,7 +4,10 @@ import (
 	types "github.com/dell/goscaleio/types/v1"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
@@ -17,6 +20,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 			Description:         "Unique identifier of the protection domain instance.",
 			MarkdownDescription: "Unique identifier of the protection domain instance.",
 			Computed:            true,
+			PlanModifiers: []planmodifier.String{
+				stringplanmodifier.UseStateForUnknown(),
+			},
 		},
 		"name": schema.StringAttribute{
 			Description:         "Unique name of the protection domain instance.",
@@ -30,6 +36,7 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 			Optional:            true,
 			PlanModifiers: []planmodifier.Bool{
 				boolDefault(true),
+				boolplanmodifier.UseStateForUnknown(),
 			},
 		},
 		"state": schema.StringAttribute{
@@ -47,6 +54,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 			MarkdownDescription: "Whether SDS Rf Cache is enabled or not.",
 			Computed:            true,
 			Optional:            true,
+			PlanModifiers: []planmodifier.Bool{
+				boolplanmodifier.UseStateForUnknown(),
+			},
 		},
 		"rf_cache_operational_mode": schema.StringAttribute{
 			Description: "Operational Mode of the SDS RF Cache." +
@@ -65,6 +75,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 					string(types.PDRCModeWriteMiss),
 				),
 			},
+			PlanModifiers: []planmodifier.String{
+				stringplanmodifier.UseStateForUnknown(),
+			},
 		},
 		"rf_cache_page_size_kb": schema.Int64Attribute{
 			Description: "Page size of the SDS RF Cache in KB." +
@@ -73,6 +86,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" Can be set only when `rf_cache_enabled` is set to `true`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"rf_cache_max_io_size_kb": schema.Int64Attribute{
 			Description: "Maximum IO of the SDS RF Cache in KB." +
@@ -81,6 +97,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" Can be set only when `rf_cache_enabled` is set to `true`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"fgl_default_num_concurrent_writes": schema.Int64Attribute{
 			Description:         "Fine Granularity default number of concurrent writes.",
@@ -92,6 +111,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 			MarkdownDescription: "Whether Fine Granularity Metadata Cache is enabled or not.",
 			Computed:            true,
 			Optional:            true,
+			PlanModifiers: []planmodifier.Bool{
+				boolplanmodifier.UseStateForUnknown(),
+			},
 		},
 		"fgl_default_metadata_cache_size": schema.Int64Attribute{
 			Description: "Fine Granularity Metadata Cache size." +
@@ -100,6 +122,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" Can be set only when `fgl_metadata_cache_enabled` is set to `true`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"protected_maintenance_mode_network_throttling_in_kbps": schema.Int64Attribute{
 			Description: "Maximum allowed IO for protected maintenance mode in KBps." +
@@ -108,6 +133,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" The value `0` represents unlimited bandwidth. The default value is `0`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"rebuild_network_throttling_in_kbps": schema.Int64Attribute{
 			Description: "Maximum allowed IO for rebuilding in KBps." +
@@ -116,6 +144,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" The value `0` represents unlimited bandwidth. The default value is `0`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"rebalance_network_throttling_in_kbps": schema.Int64Attribute{
 			Description: "Maximum allowed IO for rebalancing in KBps." +
@@ -124,6 +155,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" The value `0` represents unlimited bandwidth. The default value is `0`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"vtree_migration_network_throttling_in_kbps": schema.Int64Attribute{
 			Description: "Maximum allowed IO for vtree migration in KBps." +
@@ -132,6 +166,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" The value `0` represents unlimited bandwidth. The default value is `0`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"overall_io_network_throttling_in_kbps": schema.Int64Attribute{
 			Description: "Maximum allowed IO for protected maintenance mode in KBps. Must be greater than any other network throttling parameter." +
@@ -140,6 +177,9 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 				" The value `0` represents unlimited bandwidth. The default value is `0`.",
 			Computed: true,
 			Optional: true,
+			PlanModifiers: []planmodifier.Int64{
+				int64planmodifier.UseStateForUnknown(),
+			},
 		},
 		"replication_capacity_max_ratio": schema.Int64Attribute{
 			Description:         "Maximum Replication Capacity Ratio.",
@@ -150,6 +190,11 @@ var ProtectionDomainResourceSchema schema.Schema = schema.Schema{
 			Description:         "Underlying REST API links.",
 			MarkdownDescription: "Underlying REST API links.",
 			Computed:            true,
+			// TODO: there is a bug in listplanmodifier which is fixed in tpf-v1.2.0
+			// https://github.com/hashicorp/terraform-plugin-framework/issues/644
+			// PlanModifiers: []planmodifier.List{
+			// 	listplanmodifier.UseStateForUnknown(),
+			// },
 			NestedObject: schema.NestedAttributeObject{
 				Attributes: map[string]schema.Attribute{
 					"rel": schema.StringAttribute{
