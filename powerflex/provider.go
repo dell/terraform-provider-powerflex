7995003a8a99d0cf8a750071af88f9765a626c2e
Akash Shendge <60608990+shenda1@users.noreply.github.com>
2023-05-02 17:07:47 +0530

    added create/read/delete operation for device resource (#65)

--- powerflex/provider.go
+++ powerflex/provider.go
@@ -251,5 +251,6 @@ func (p *powerflexProvider) Resources(_ context.Context) []func() resource.Resou
 		SDCResource,
 		StoragepoolResource,
 		NewSDCVolumesMappingResource,
+		NewDeviceResource,
 	}
 }
