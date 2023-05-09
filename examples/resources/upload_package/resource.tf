b676f5ee1d8ee4740d32e22ebd7bb68f86ea32a2
Krunal-Thakkar <krunal.thakkar@dell.com>
2023-05-09 05:36:27 -0400

    upload package resource

--- examples/resources/upload_package/resource.tf
+++ examples/resources/upload_package/resource.tf
@@ -1,3 +1,3 @@
 resource "powerflex_uploadPackage" "upload-package-1" {
-  file_path = "/home/krunal/Work/Software/EMC-ScaleIO-lia-3.6-700.103.Ubuntu.22.04.x86_64.tar"
+  file_path = ["/home/krunal/Work/Software/PowerFlex_3.6.700.103_Ubuntu22.04/EMC-ScaleIO-lia-3.6-700.103.Ubuntu.22.04.x86_64.tar"]
 }
