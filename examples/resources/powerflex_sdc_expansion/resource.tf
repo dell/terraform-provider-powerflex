f1036432c5c21f4f7dffc96612aa581669bee0bc
root <krunal.thakkar@dell.com>
2023-05-18 15:04:15 -0400

    tf update

--- examples/resources/powerflex_sdc_expansion/resource.tf
+++ examples/resources/powerflex_sdc_expansion/resource.tf
@@ -1,12 +1,5 @@
 # Command to run this tf file : terraform init && terraform plan && terraform apply
 # Create, Update, Delete is supported for this resource
-# To import , check volume_resource_import.tf for more info
-# To create / update, either storage_pool_id or storage_pool_name must be provided
-# Also , to create / update, either protection_domain_id or protection_domain_name must be provided
-# name, size is the required parameter to create or update
-# other  atrributes like : capacity_unit, volume_type, use_rm_cache, compression_method, access_mode, remove_mode, sdc_list are optional 
-# To check which attributes of the snapshot can be updated, please refer Product Guide in the documentation
-
 resource "powerflex_sdc_expansion" "test-csv2" {
 	mdm_ip = "10.247.103.160"
 	mdm_password = "ABCD"
