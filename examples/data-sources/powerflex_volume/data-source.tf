7918531404a012f97b894146e3f762c29ddbd093
Anika Agiwal <anika.agiwal@dell.com>
2023-09-18 05:34:31 -0400

    added docs for sdc-volume-mapping

--- examples/data-sources/powerflex_volume/data-source.tf
+++ examples/data-sources/powerflex_volume/data-source.tf
@@ -15,17 +15,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-# commands to run this tf file : terraform init && terraform apply --auto-approve
 # This datasource reads volumes either by id or name or storage_pool_id or storage_pool_name where user can provide a value to any one of them
 # If it is an empty datsource block , then it will read all the volumes
 # If id or name is provided then it reads a particular volume with that id or name
 # If storage_pool_id or storage_pool_name is provided then it will return the volumes under that storage pool
-# Only one of the attribute can be provided among id, name, storage_pool_id, storage_pool_name 
 
 data "powerflex_volume" "volume" {
-
-  # This datasource reads volumes either by id or name or storage_pool_id or storage_pool_name where user can provide a value to any one of them
-  # If it is an empty datsource block , then it will read all the volumes
 
   id = "4570761d00000024"
   #name = "cosu-ce5b8a2c48"
