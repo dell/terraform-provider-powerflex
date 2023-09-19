11f6f745ecc2bb4864fb630394ea2ecaf2ee2d18
root <krunal.thakkar@dell.com>
2023-09-19 03:21:54 -0400

    Doc changes

--- examples/resources/powerflex_cluster/import.sh
+++ examples/resources/powerflex_cluster/import.sh
@@ -1,8 +1,15 @@
-# Below are the steps to import Cluster :
-# Step 1 - To import a Cluster , we need the MDM IP, MDM Password, LIA Password  of that Cluster 
-# Step 2 - create a tf file with empty resource block . Refer the example below.
-# Example :
-# resource "powerflex_cluster" "resource_block_name" {
-# }
-# Step 3 - execute the command : terraform init && terraform import "powerflex_cluster.resource_block_name" "MDM_IP,MDM_Password,LIA_Password" (resource_block_name must be taken from 2)
-# Step 4 - After successful execution of the command , check the state file
+# /*
+# Copyright (c) 2023 Dell Inc., or its subsidiaries. All Rights Reserved.
+# Licensed under the Mozilla Public License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://mozilla.org/MPL/2.0/
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# */
+
+# import existing cluster
+terraform import powerflex_cluster.cluster_data "<MDM_IP>,<MDM_Password>,<LIA_Password>"
