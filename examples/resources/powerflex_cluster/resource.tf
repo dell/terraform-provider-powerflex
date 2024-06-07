7b7d79920503f956c449a766712b94e9813bab50
root <krunal.thakkar@dell.com>
2024-06-07 08:06:51 -0400

    Check

--- examples/resources/powerflex_cluster/resource.tf
+++ examples/resources/powerflex_cluster/resource.tf
@@ -1,109 +1,131 @@
-terraform {
-  required_providers {
-    powerflex = {
-      version = "1.5.0"
-      source  = "registry.terraform.io/dell/powerflex"
-    }
-  }
-}
-provider "powerflex" {
-  username = "admin"
-  password = "Password123@"
-  endpoint = "https://pflex4env12.pie.lab.emc.com"
-  insecure = true
-  timeout  = 120
-}
+/*
+Copyright (c) 2023-2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
 
 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+# Command to run this tf file : terraform init && terraform plan && terraform apply.
+# Create, Read, Delete and Import operations are supported for this resource.
+
+# Example for deploying cluster. After successful execution, 3 node MDM cluster will be deployed with 3 SDCs and 2 SDS.
 resource "powerflex_package" "upload-test" {
-  file_path = ["/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-lia-4.5-0.287.sles15.3.x86_64.rpm",
-    "/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-mdm-4.5-0.287.sles15.3.x86_64.rpm",
-    "/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-sdc-4.5-0.287.sles15.3.x86_64.rpm",
-    "/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-sds-4.5-0.287.sles15.3.x86_64.rpm",
-    "/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-sdr-4.5-0.287.sles15.3.x86_64.rpm",
-  "/root/powerflex_packages/PowerFlex_4.5.0.287_SLES15.3/EMC-ScaleIO-activemq-5.16.4-62.noarch.rpm"]
+  file_path = ["/root/powerflex_packages/PowerFlex_3.6.700.103_RHEL_OEL7/EMC-ScaleIO-lia-3.6-700.103.el7.x86_64.rpm",
+    "/root/powerflex_packages/PowerFlex_3.6.700.103_RHEL_OEL7/EMC-ScaleIO-mdm-3.6-700.103.el7.x86_64.rpm",
+    "/root/powerflex_packages/PowerFlex_3.6.700.103_RHEL_OEL7/EMC-ScaleIO-sds-3.6-700.103.el7.x86_64.rpm",
+    "/root/powerflex_packages/PowerFlex_3.6.700.103_RHEL_OEL7/EMC-ScaleIO-sdc-3.6-700.103.el7.x86_64.rpm",
+    "/root/powerflex_packages/PowerFlex_3.6.700.103_RHEL_OEL7/EMC-ScaleIO-sdr-3.6-700.103.el7.x86_64.rpm"]
 }
 
 resource "powerflex_cluster" "test" {
+
   depends_on   = [powerflex_package.upload-test]
-  mdm_password = "Password123@"
-  lia_password = "Password123@"
+
+  # Security Related Field
+  mdm_password = "Password"
+  lia_password = "Password"
+
   # Advance Security Configuration
   allow_non_secure_communication_with_lia = false
   allow_non_secure_communication_with_mdm = false
   disable_non_mgmt_components_auth        = false
-  # Cluster Configuration related fields
+
+  # Cluster Configuration related fields 
   cluster = [
     {
-      # MDM Configuration Fields
-      username         = "root",
-      password         = "admin",
-      operating_system = "linux",
-      is_mdm_or_tb     = "Primary",
-      ips          = "10.247.103.160" #,10.247.39.124,10.247.78.174", 
-      # mdm_mgmt_ip              = "10.247.103.160",
-      mdm_name             = "mdm1",
+      # MDM Configuration Fields 
+      ips                  = "10.10.10.1",
+      username             = "root",
+      password             = "Password",
+      operating_system     = "linux",
+      is_mdm_or_tb         = "primary",
+      mdm_ips              = "10.10.10.1",
+      mdm_mgmt_ip          = "10.10.10.1",
+      mdm_name             = "MDM_1",
       perf_profile_for_mdm = "HighPerformance",
-      # virtual_ips              = "10.247.39.126,10.247.78.177",
-      # virtual_ip_nics          = "eth1,eth2",
-      is_sds = "Yes",
-      #   sds_name                 = "sds1",
-      #   sds_all_ips              = "10.247.39.124,10.247.78.174",
-      #   protection_domain        = "domain_1",
-      #  sds_storage_device_list  = "/dev/sdb",
-      #  sds_storage_device_names = "sdb",
-      #  storage_pool_list        = "pool1",
-      #  perf_profile_for_sds     = "HighPerformance",
-      is_sdc = "No"
+      virtual_ips          = "10.30.30.1",
+      virtual_ip_nics      = "ens192",
+
+      # SDS Configuration Fields
+      is_sds      = "yes",
+      sds_name    = "sds1",
+      sds_all_ips = "10.20.20.3", # conflict with sds_to_sds_only_ips,sds_to_sdc_only_ips
+      # sds_to_sdc_only_ips      = "10.20.20.2", 
+      # sds_to_sds_only_ips      = "10.20.20.1",
+      fault_set                = "fs1",
+      protection_domain        = "domain_1"
+      sds_storage_device_list  = "/dev/sdb"
+      sds_storage_device_names = "device1"
+      storage_pool_list        = "pool1"
+      perf_profile_for_sds     = "HighPerformance"
+
+      # SDC Configuration Fields
+      is_sdc               = "yes",
+      sdc_name             = "sdc1",
+      perf_profile_for_sdc = "HighPerformance",
+
+      # Rfcache Configuration Fields
+      is_rfcache               = "No",
+      rf_cache_ssd_device_list = "/dev/sdd"
+
+      # SDR Configuration Fields
+      is_sdr   = "Yes",
+      sdr_name = "SDR_1"
+      sdr_port = "2000"
+      # sdr_application_ips  = "10.20.30.1"
+      # sdr_storage_ips      = "10.20.30.2"
+      # sdr_external_ips     = "10.20.30.3" 
+      sdr_all_ips          = "10.10.20.1" # conflict with sdr_application_ips, sdr_storage_ips, sdr_external_ips
+      perf_profile_for_sdr = "Compact"
     },
     {
-      username         = "root",
-      password         = "admin",
-      operating_system = "linux",
-      is_mdm_or_tb     = "Secondary",
-      mdm_name         = "mdm2",
-      ips          = "10.247.103.161" #,10.247.39.122,10.247.78.175", 
-      # mdm_mgmt_ip              = "10.247.103.161",
-      perf_profile_for_mdm = "HighPerformance",
-      # virtual_ips              = "10.247.39.126,10.247.78.177",
-      # virtual_ip_nics          = "eth1,eth2",
-      is_sds = "Yes",
-      #  sds_name                 = "sds2",
-      # sds_all_ips              = "10.247.39.122,10.247.78.175",
-      # sds_storage_device_list  = "/dev/sdb",
-      # sds_storage_device_names = "sdb",
-      # protection_domain        = "domain_1",
-      # storage_pool_list        = "pool1",
-      # perf_profile_for_sds     = "HighPerformance",
-      is_sdc = "No"
+      ips                     = "10.10.10.2",
+      username                = "root",
+      password                = "Password",
+      operating_system        = "linux",
+      is_mdm_or_tb            = "Secondary",
+      protection_domain       = "domain_1"
+      sds_storage_device_list = "/dev/sdb"
+      storage_pool_list       = "pool1"
+      is_sds                  = "yes",
+      sds_name                = "sds2",
+      is_sdc                  = "yes",
+      sdc_name                = "sdc2",
+      perf_profile_for_sdc    = "compact",
+      is_rfcache              = "No",
+      is_sdr                  = "No",
     },
     {
-      username         = "root",
-      password         = "admin",
-      operating_system = "linux",
-      is_mdm_or_tb     = "TB",
-      mdm_name         = "tb1",
-      ips              = "10.247.103.162" #,10.247.39.130,10.247.78.176",
-      # mdm_mgmt_ip              = "10.247.103.162",
-      perf_profile_for_mdm = "HighPerformance",
-      is_sds               = "Yes",
-      # sds_name                 = "sds3",
-      # sds_all_ips              = "10.247.39.130,10.247.78.176",
-      # sds_storage_device_list  = "/dev/sdb",
-      # sds_storage_device_names = "sdb",
-      # protection_domain        = "domain_1",
-      # storage_pool_list        = "pool1",
-      # perf_profile_for_sds     = "HighPerformance",
-      is_sdc = "No"
-    }
+      ips                  = "10.10.10.3",
+      username             = "root",
+      password             = "Password",
+      operating_system     = "linux",
+      is_mdm_or_tb         = "TB",
+      is_sds               = "No",
+      is_sdc               = "yes",
+      sdc_name             = "sdc3",
+      perf_profile_for_sdc = "compact",
+      is_rfcache           = "No",
+      is_sdr               = "No",
+    },
   ]
+  # Storage Pool Configuration Fields
   storage_pools = [
     {
-      media_type = "HDD"
-      # protection_domain = "domain_1"
-      # storage_pool      = "pool1"
-      # daya_layout       = "MG"
-      # zero_padding      = "true"
+      media_type                              = "HDD"
+      protection_domain                       = "domain_1"
+      storage_pool                            = "pool1"
+      replication_journal_capacity_percentage = "50"
     }
   ]
 }
--- examples/resources/powerflex_firmware_repository/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "CIFS_SHARE_Location" {
-  type        = string
-  default = "\\\\10.247.39.136\\SharedFolder\\PowerFlex_Software_4.5.0.0_287_r1.zip"
-}
-
-variable "HTTP_Location" {
-  type        = string
-  default = "https://100.65.27.72/artifactory/vxfm-yum-release/pfmp20/RCM/Denver/RCMs/SoftwareOnly/PowerFlex_Software_4.5.0.0_287_r1.zip"
-}
