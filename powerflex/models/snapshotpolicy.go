73c4fafb196324b16cf69aff5a70c507cc90d520
Akash Shendge <60608990+shenda1@users.noreply.github.com>
2024-06-24 19:09:41 +0530

    addressing checkmarx issues (#197)
    * addressing checkmarx issues
    
    Signed-off-by: Shendge <akash.shendge@dell.com>
    
    * winrm Fixes
    
    ---------
    
    Signed-off-by: Shendge <akash.shendge@dell.com>
    Co-authored-by: root <krunal.thakkar@dell.com>

--- powerflex/models/snapshotpolicy.go
+++ powerflex/models/snapshotpolicy.go
@@ -62,7 +62,7 @@ type SnapshotPolicyLinkModel struct {
 type SnapshotPolicyResourceModel struct {
 	ID                               types.String   `tfsdk:"id"`
 	Name                             types.String   `tfsdk:"name"`
-	NumOfRetainedSnapshotsPerLevel   []types.Int64  `tfsdk:"num_of_retained_snapshots_per_level"`
+	NumOfRetainedSnapshotsPerLevel   []int64        `tfsdk:"num_of_retained_snapshots_per_level"`
 	AutoSnapshotCreationCadenceInMin types.Int64    `tfsdk:"auto_snapshot_creation_cadence_in_min"`
 	Paused                           types.Bool     `tfsdk:"paused"`
 	VolumeIds                        []types.String `tfsdk:"volume_ids"`
