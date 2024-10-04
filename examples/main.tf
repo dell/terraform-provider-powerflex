823ef07a3d1dcec6cb65cbaf152ca45418f7572c
cdoria <connor.doria@dell.com>
2024-10-04 07:52:52 -0400

    Add Main and README to Examples

---
+++ examples/main.tf
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+terraform {
+  required_providers {
+    powerflex = {
+      source  = "registry.terraform.io/dell/powerflex"
+    }
+  }
+}
+
+variable "username" {
+  type        = string
+}
+
+variable "password" {
+  type        = string
+}
+
+variable "endpoint" {
+  type        = string
+}
+
+provider "powerflex" {
+  username = var.username
+  password = var.password
+  endpoint = var.endpoint
+  insecure = true
+  timeout  = 120
+}
